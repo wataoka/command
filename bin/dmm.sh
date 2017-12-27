ca5a7bdb36a3323f68475ef276fde50f781fb4c5
Koki Wataoka <contact@wataoka.com>
2017-12-27 16:05:29 +0900

    dmmコマンド追加

---
+++ bin/dmm.sh
@@ -0,0 +1,5 @@
+
+xdg-open https://eikaiwa.dmm.com
+xdg-open https://google.dmm.com
+xdg-open https://www.google.co.jp/search?client=ubuntu&channel=fs&q=%E7%BF%BB%E8%A8%B3&ie=utf-8&oe=utf-8&gfe_rd=cr&dcr=0&ei=M0RDWvXRNeKQ8Qee4Kr4CA
+xdg-open https://www.google.co.jp/search?client=ubuntu&channel=fs&q=%E7%BF%BB%E8%A8%B3&ie=utf-8&oe=utf-8&gfe_rd=cr&dcr=0&ei=M0RDWvXRNeKQ8Qee4Kr4CA
